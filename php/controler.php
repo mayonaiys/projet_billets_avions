51281c2a573f4ae3f6c27489f5a0266471393eef
mayonaiys <49065703+mayonaiys@users.noreply.github.com>
2020-06-09 15:49:59 +0200

    Premiet jet fonction getAvailableFlights()

--- php/controler.php
+++ php/controler.php
@@ -18,34 +18,78 @@ function connexbdd($base,$user,$password){
 }
 
 function getAvailableFlights($json){
+
     //Décodage du fichier json
     $data = json_decode($json,true);
 
     //Connexion à la base de données
     $bdd = connexbdd(DB_PATH,DB_USER,DB_PASSWORD);
 
+    //Récupération de la date du jour
+    $date = getdate();
+    $date = "".$date['year']."-".$date['mon']."-".$date['mday'];
+    $date = new DateTime($date);
+    $dateDeparture = $data['depDate'];
+    $dateDeparture = new DateTime($dateDeparture);
+    $interval = $date->diff($dateDeparture);
+    $interval =  $interval->days;
+
+    //On cherche à quel jour de la semaine correspond la date demandée par le client
+    $dateDeparture = $data['depDate'];
+    list($yDep,$mDep,$dDep) = explode("-", $dateDeparture);
+    $timeStamp = mktime(0,0,0,$mDep,$dDep,$yDep);
+    $day = date('w', $timeStamp);
+
+
     //Requêtes en fonctions des données insérées par l'utilisateur
-    $request = $bdd->prepare('SELECT * FROM flights WHERE originCity=:depCity AND destinationCity=:arrivalCity AND dayOfWeek=:depDate');
+    $request = $bdd->prepare('SELECT * FROM flights WHERE originCity=:depCity AND destinationCity=:arrivalCity AND dayOfWeek=:dayOfWeek');
     $request->bindParam(':depCity', $data['depCity'], PDO::PARAM_STR);
     $request->bindParam(':arrivalCity', $data['arrivalCity'], PDO::PARAM_STR);
-    $request->bindParam(':depDate', $data['depDate'], PDO::PARAM_STR);
+    $request->bindParam(':dayOfWeek', $day, PDO::PARAM_INT);
     $request->execute();
+
+    $newJson = array();
     while(($response = $request->fetch())!=0){
-        echo '<p>'.$response['ID']." | ".$response['dayOfWeek']." | ".$response['departureTime']." | ".$response['arrivalTime'].'</p>';
-        $requestFares = $bdd->prepare('SELECT * FROM fares WHERE route=:route');
-        $requestFares->bindParam(':route', $response['route'], PDO::PARAM_STR);
-        echo $response['route'];
-        while (($responseFares = $requestFares->fetch()) != 0){
-            echo '<p>test</p>';
+        $fareRequest = $bdd->prepare('SELECT fare FROM fares WHERE route=:route AND dateToDeparture=:dateDep AND weFlights=:weFlights');
+        $fareRequest->bindParam(':route', $response['route'], PDO::PARAM_STR);
+        $fareRequest->bindParam(':dateDep', $interval, PDO::PARAM_INT);
+        if($day >= 0 && $day <= 4){
+            $weFlights = 0;
+        } else {
+            $weFlights = 1;
         }
+        $fareRequest->bindParam(':weFlights',$weFlights, PDO::PARAM_INT);
+        $fareRequest->execute();
+        if(($fare = $fareRequest->fetch()['fare'])!=0){
+            echo '<p>'.$response['ID']." | ".$response['route']." | ".resolveDay($response['dayOfWeek'])." | ".$response['departureTime']." | ".$response['arrivalTime']." | ".($fare/2)." | ".$fare." | ".($data['nbrAdults']*$fare+$data['nbrChildren']*$fare/2).'</p>';
+            $array = array("ID"=>$response['ID'],"route"=>$response['route'],"date"=>resolveDay($response['dayOfWeek']),"depTime"=>$response['departureTime'],"arrivalTime"=>$response['arrivalTime'],"childrenFare"=>($fare/2),"adultsFare"=>$fare,"totalFare"=>($data['nbrAdults']*$fare+$data['nbrChildren']*$fare/2));
+            array_push($newJson,$array);
+        }
+    }
+    $newData = json_encode($newJson); //Encodage de la réponse
+    print_r($newData);
+}
+
+function resolveDay($day){
+    switch ($day) {
+        case 0:
+            return "Lundi";
+        case 1:
+            return "Mardi";
+        case 2:
+            return "Mercredi";
+        case 3:
+            return "Jeudi";
+        case 4:
+            return "Vendredi";
+        case 5:
+            return "Samedi";
+        case 6:
+            return "dimanche";
     }
 }
 
-function test(){
-
-}
-
-$json ='{"depCity" : "Edmonton", "arrivalCity" : "Quebec", "nbrPassengers" : 5, "depDate" : "0", "minPrice" : "min", "maxPrice" : "max"}';
+$json ='{"depCity" : "villeune", "arrivalCity" : "villedeux", "nbrAdults" : 5, "nbrChildren" : 2, "depDate" : "2020-06-29", "minPrice" : "min", "maxPrice" : "max"}';
 getAvailableFlights($json);
 
 ?>
