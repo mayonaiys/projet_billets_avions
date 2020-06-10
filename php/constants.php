9e12d1b8c81e106e8ffe27c4301f2f9449aa8484
Charlotte49 <cheudu49@yahoo..fr>
2020-06-10 12:15:31 +0200

    commit pour enlever conflit

--- php/constants.php
+++ php/constants.php
@@ -1,6 +1,6 @@
 <?php
-  define('DB_USER', '...');
-  define('DB_PASSWORD', '...');
-  define('DB_NAME', '...');
+  define('DB_USER', 'mysql');
+  define('DB_PASSWORD', 'isen2018');
+  define('DB_NAME', 'projetCir2');
   define('DB_SERVER', 'localhost');
 ?>
